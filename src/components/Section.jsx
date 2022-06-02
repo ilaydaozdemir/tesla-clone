0bbf03a5a644315c6648a1a177d7961b589fcfd5
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-06-02 11:35:27 +0300

    Add Fade effect

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 const Section = ({
   title,
@@ -10,10 +11,12 @@ const Section = ({
 }) => {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
         <ButtonGroup>
           <LeftButton>{leftBtnText}</LeftButton>
